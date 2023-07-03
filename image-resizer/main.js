cfe6b6189edbb7acae9a192e4470d031f5803fde
Robert <t2000ro09bi28@gmail.com>
2023-07-03 22:52:18 +0200

    Init (Base render + menu)

---
+++ image-resizer/main.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: isDev ? 1000 : 500,
+        height: 600
+    });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Image Resizer',
+        width: 300,
+        height: 300
+    });
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+});
+
+const menu = [
+    ...(isMac 
+        ? [
+            {
+                label: app.name,
+                submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow
+                    }
+                ]
+            }
+        ]
+        : []),
+    {
+        role: 'fileMenu',
+    },
+    ...(!isMac ? [{
+        label: 'Help',
+        submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow
+                    }
+                ]
+    }] : []),
+];
+
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit();
+    }
+});
